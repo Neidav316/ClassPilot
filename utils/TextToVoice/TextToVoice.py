8c28d7bb9ab74d986e5dba2770276c96f8879ae2
Neidav316 <neidav86@gmail.com>
2024-07-14 15:24:12 +0300

    fixed the tts using pygame

--- utils/TextToVoice/TextToVoice.py
+++ utils/TextToVoice/TextToVoice.py
@@ -1,47 +1,57 @@
 from gtts import gTTS
 import os
-from pydub import AudioSegment
-import simpleaudio as sa
+import pygame
+import numpy as np
 
 DIR_PATH = os.path.abspath(os.path.curdir)
 
+class MP3Player:
+    def __init__(self):
+        pygame.mixer.init()
+
+    def play(self, file_path):
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+
+    def stop(self):
+        pygame.mixer.music.stop()
+        pygame.mixer.music.unload()
+    def is_playing(self):
+        return pygame.mixer.music.get_busy()
+
 class TextToVoice:
     def __init__(self,name="output"):
-        self.audio_file = f"{name}.wav"
-        self.audio_wave_object = None
-        self.play_obj = None
+        self.audio_file = f"{name}.mp3"
+        self.play_obj = MP3Player()
 
     def generate_speech(self, text, lang='en', tld='com', slow=False, lang_check=True, name=None):
-        AudioSegment.converter = DIR_PATH+r"\ffmpeg.exe"
         tts = gTTS(text=text, lang=lang, tld=tld, slow=slow, lang_check=lang_check)
         if name is None:
-            name = 'output.mp3'
-        else:
-            name = name + '.mp3'
-        name = DIR_PATH + name
-        tts.save(name)
-        sound = AudioSegment.from_mp3(name)
-        sound.export(self.audio_file, format="wav")
-        os.remove(name)
-        self.audio_wave_object = sa.WaveObject.from_wave_file(self.audio_file)
+            name = 'output'
+        self.audio_file = DIR_PATH + f"\\{name}.mp3"
+        tts.save(self.audio_file)
 
     def play(self, text, lang='en'):
         self.generate_speech(text, lang=lang)
-        if self.play_obj is not None:
+        if self.play_obj.is_playing():
             self.play_obj.stop()
-        self.play_obj = self.audio_wave_object.play()
-        self.play_obj.wait_done()
-
+        self.play_obj.play(self.audio_file)
         self.remove()
 
-
     def stop(self):
-        if self.play_obj is not None:
+        if self.play_obj.is_playing():
             self.play_obj.stop()
-
         self.remove()
 
     def remove(self):
         try:
+            while self.play_obj.is_playing():
+                pass
+            self.play_obj.stop()
             os.remove(self.audio_file)
-        except:pass
+        except Exception as e:
+            print(e)
+
+
+
+
